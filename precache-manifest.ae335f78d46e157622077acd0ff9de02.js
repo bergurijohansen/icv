f547e0b619cf8b7a7be3fc389082b5aa334aeb32
bergurijohansen <bergurij@gmail.com>
2019-08-12 21:05:30 +0100

    Updates

---
+++ precache-manifest.ae335f78d46e157622077acd0ff9de02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31e5392a4a1331922f1fb56036cd8b60",
+    "url": "/icv/index.html"
+  },
+  {
+    "revision": "e18e69d07b251ea6082d",
+    "url": "/icv/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d1e0f1eb389feaac9d2e",
+    "url": "/icv/static/js/2.a5bfbcb0.chunk.js"
+  },
+  {
+    "revision": "e18e69d07b251ea6082d",
+    "url": "/icv/static/js/main.9d7ffcd5.chunk.js"
+  },
+  {
+    "revision": "5ce0df90250a48e95d21",
+    "url": "/icv/static/js/runtime~main.d98e8eae.js"
+  }
+]);
